e0ea7bf5d81257d230aa19877a2558096d359a97
wjj <wjj99@qq.com>
2020-06-12 18:01:50 +0800

    测试

--- app/src/main/java/trust/web3provider/MainActivityWebview2.java
+++ app/src/main/java/trust/web3provider/MainActivityWebview2.java
@@ -59,7 +59,6 @@ public class MainActivityWebview2 extends AppCompatActivity {
         webSettings.setDomStorageEnabled(true);//允许android调用javascript
 
 
-
         //android 5.0以上不允许WebView加载http的图片，通过以下设置可以解决
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
@@ -72,10 +71,19 @@ public class MainActivityWebview2 extends AppCompatActivity {
             @Override
             public void onLoadResource(WebView webView, String s) {
                 super.onLoadResource(webView, s);
-                // hidenBanner(webView);
+                 //hidenBanner(webView);
+
 
             }
+
+            @Override
+            public void onPageStarted(WebView webView, String url, Bitmap favicon) {
+                inputJS(webView);
+            }
         });
+
+
+
 
         mWebView.setWebChromeClient(new WebChromeClient() {
             @Override
@@ -96,19 +104,18 @@ public class MainActivityWebview2 extends AppCompatActivity {
 
         });
 
-        //必须和js函数名字一致，注册好具体执行回调函数，类似java实现类。
+        //必须和js函数名字一致，注册好具体执行回调函数，类似java实现类。 submitFromWeb
+
+
+        //js调用安卓的requestAccounts;安卓调用js的 window.ethereum.setAddress(addr)方法
         mWebView.registerHandler("requestAccounts", new BridgeHandler() {
 
             @Override
             public void handler(String data, CallBackFunction function) {
+                Toast.makeText(MainActivityWebview2.this, "js调用安卓的requestAccounts", Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "js调用安卓的requestAccounts");
+                //pickFile();
 
-                String str = "这是html返回给java的数据:" + data;
-                // 例如你可以对原始数据进行处理
-                str = str + ",Java经过处理后截取了一部分：" + str.substring(0, 5);
-                Log.i(TAG, "handler = requestAccounts, data from web = " + data);
-                Toast.makeText(MainActivityWebview2.this, str, Toast.LENGTH_SHORT).show();
-                //回调返回给Js
-                function.onCallBack(str + ",Java经过处理后截取了一部分：" + str.substring(0, 5));
             }
 
         });
@@ -129,37 +136,75 @@ public class MainActivityWebview2 extends AppCompatActivity {
     }
 
     private void inputJS(WebView view) {
-        //隐藏元素
+
         String jsStr = "";
 
+        long time1=System.currentTimeMillis();
         try {
             Resources res = getResources();
-            InputStream in = res.openRawResource(trust.web3provider.R.raw.trust_min);
+            InputStream in = res.openRawResource(trust.web3provider.R.raw.trustmin5);
 
             byte[] b = new byte[in.available()];
             int readLen = in.read(b);
-            jsStr = String.format("Len: %1$s\n%2$s", readLen, new String(b));
+            jsStr = new String(b);//; String.format("Len: %1$s\n%2$s", readLen, new String(b));
 
-            ((TextView) findViewById(R.id.out)).setText(jsStr);
+            // ((TextView) findViewById(R.id.out)).setText(jsStr);
+            Log.e(TAG, jsStr);
         } catch (Exception e) {
-            ((TextView) findViewById(R.id.out)).setText(e.getMessage());
+            // ((TextView) findViewById(R.id.out)).setText(e.getMessage());
+            Log.e(TAG, "加载错误 " + e.getMessage());
         }
-        view.loadUrl(jsStr);//创建方法
+
+        long time2=System.currentTimeMillis();
+        Log.e(TAG,"时间："+(time2-time1)/1000);
+
+       // view.loadUrl(jsStr);//创建方法
+        view.evaluateJavascript(jsStr, new ValueCallback<String>() {//将读取的js字符串注入webview中
+            @Override
+            public void onReceiveValue(String value) {//js与native交互的回调函数
+                Log.d(TAG, "value=" + value);
+            }
+        });
 
         //String rpcURL="https://mainnet.infura.io/v3/76208b0279d342f1aa8857cb5a31e45e"; chainId:1 address:从当前钱包获取
-        view.loadUrl("(function() {\n" +
-                "     var config = {\n" +
-                "                    address: '0xEa5CDC6625416c87913180847ace2Bf42Dbb422e'.toLowerCase(),\n" +
-                "                    chainId: 1,\n" +
-                "                    rpcUrl: \'https://mainnet.infura.io/v3/76208b0279d342f1aa8857cb5a31e45e'\n" +
-                "                };\n" +
-                "                const provider = new window.Trust(config);\n" +
-                "                window.ethereum = provider;\n" +
-                "                window.web3 = new window.Web3(provider);\n" +
-                "                window.web3.eth.defaultAccount = config.address;\n" +
-                "\n" +
-                "                window.chrome = {webstore: {}};\n" +
-                "            })();");//加载方法
+//        view.loadUrl("(function() {\n" +
+//                "     var config = {\n" +
+//                "                    address: '0xEa5CDC6625416c87913180847ace2Bf42Dbb422e'.toLowerCase(),\n" +
+//                "                    chainId: 1,\n" +
+//                "                    rpcUrl: 'https://mainnet.infura.io/v3/76208b0279d342f1aa8857cb5a31e45e'\n" +
+//                "                };\n" +
+//                "                const provider = new window.Trust(config);\n" +
+//                "                window.ethereum = provider;\n" +
+//                "                window.web3 = new window.Web3(provider);\n" +
+//                "                window.web3.eth.defaultAccount = config.address;\n" +
+//                "\n" +
+//                "                window.chrome = {webstore: {}};\n" +
+//                "            })();");
+
+
+        String url2 = "";
+        try {
+            Resources res = getResources();
+            InputStream in = res.openRawResource(R.raw.plus);
+
+            byte[] b = new byte[in.available()];
+            int readLen = in.read(b);
+            url2 = new String(b);//; String.format("Len: %1$s\n%2$s", readLen, new String(b));
+
+            // ((TextView) findViewById(R.id.out)).setText(jsStr);
+           // Log.e(TAG, jsStr);
+        } catch (Exception e) {
+            // ((TextView) findViewById(R.id.out)).setText(e.getMessage());
+            Log.e(TAG, "加载错误 " + e.getMessage());
+        }
+
+       // view.loadUrl(url2);
+        view.evaluateJavascript(url2, new ValueCallback<String>() {//将读取的js字符串注入webview中
+            @Override
+            public void onReceiveValue(String value) {//js与native交互的回调函数
+                Log.d(TAG, "value=" + value);
+            }
+        });
 
 
     }
